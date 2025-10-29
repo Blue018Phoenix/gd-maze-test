3eba4746958c16074a744ea025029b5512875c18
Star <48496899+Blue018Phoenix@users.noreply.github.com>
2025-10-28 20:41:58 -0400

    Add standalone character object.

--- mazeproject/dummymain.gd
+++ mazeproject/dummymain.gd
@@ -1,9 +1,11 @@
 extends Node
 
-var CurrentWorld = get_tree().current_scene
+@onready var CurrentWorld = get_tree().current_scene
 
 func AddScene(dummy): # Loads a node from disk async
 	await get_tree().create_timer(0.0).timeout # Use a 0 timer to simulate the async loading aspect of Main.AddScene(Obj , Location : Vector2, Name : String, Defered : bool)
 
 func GetCharacter(dummy): # Gets the specific party member based on a int value
-	return null
+	if CurrentWorld.has_node("ObjPlayer"):
+		return CurrentWorld.get_node("ObjPlayer")
+	return null 
